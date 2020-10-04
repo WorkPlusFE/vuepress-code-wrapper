cb051b0999952f5a19eb1e7185f4bf7c6a1a7255
hejianxian <531601727@qq.com>
2020-10-04 23:05:30 +0800

    chore: v1.1.3

--- dist/vuepress-code-wrapper.ssr.js
+++ dist/vuepress-code-wrapper.ssr.js
@@ -83,6 +83,7 @@ var script = {
         return value.split('.').length === 2;
       }
     },
+    customLink: string,
     qrcodeTitle: {
       type: String,
       default: '使用 WorkPlus 扫一扫'
@@ -118,13 +119,19 @@ var script = {
   },
   computed: {
     qrcodeLink: function qrcodeLink() {
-      if (this.qrcode && this.fn) {
-        var _this$fn$split = this.fn.split('.'),
-            _this$fn$split2 = _slicedToArray(_this$fn$split, 2),
-            hook = _this$fn$split2[0],
-            action = _this$fn$split2[1];
+      if (this.qrcode) {
+        if (this.customLink) {
+          return "".concat(this.host, "/#/").concat(this.customLink);
+        }
 
-        return "".concat(this.host, "/#/api/").concat(hook, "/").concat(action);
+        if (this.fn) {
+          var _this$fn$split = this.fn.split('.'),
+              _this$fn$split2 = _slicedToArray(_this$fn$split, 2),
+              hook = _this$fn$split2[0],
+              action = _this$fn$split2[1];
+
+          return "".concat(this.host, "/#/api/").concat(hook, "/").concat(action);
+        }
       }
 
       return '页面不存在-404！';
@@ -398,7 +405,7 @@ var __vue_staticRenderFns__ = [];
 
 var __vue_inject_styles__ = function __vue_inject_styles__(inject) {
   if (!inject) return;
-  inject("data-v-64e73c18_0", {
+  inject("data-v-15a15f16_0", {
     source: ".w6s-code-wrapper{position:relative}.w6s-code-wrapper__original-code{display:none}.w6s-code-wrapper .btns{position:absolute;right:10px;top:10px;display:flex}.w6s-code-wrapper .btns>div{width:30px;height:30px;background:#fff;margin-left:10px;font-size:10px;display:flex;justify-content:center;align-items:center;border-radius:4px;cursor:pointer;box-shadow:0 3px 10px 0 rgba(20,31,51,.08);border:1px solid #ededf0}.w6s-code-wrapper .btns>div svg{width:20px;height:20px}.w6s-code-wrapper .btns .btn__copy span,.w6s-code-wrapper .btns .btn__qrcode span{width:100%;height:100%;display:flex;justify-content:center;align-items:center}.w6s-popover{padding:18px 5px 0 5px}.w6s-popover .el-popover__title{text-align:center;margin-bottom:0;font-size:15px}.el-message{min-width:100px}",
     map: undefined,
     media: undefined
@@ -410,7 +417,7 @@ var __vue_inject_styles__ = function __vue_inject_styles__(inject) {
 var __vue_scope_id__ = undefined;
 /* module identifier */
 
-var __vue_module_identifier__ = "data-v-64e73c18";
+var __vue_module_identifier__ = "data-v-15a15f16";
 /* functional template */
 
 var __vue_is_functional_template__ = false;
